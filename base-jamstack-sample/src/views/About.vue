ee1428ab491f97093ff0030d0062186f81de73f7
Emmanuel <emmanuel.ainaj@gmail.com>
2019-10-10 12:43:44 +0100

    try again

--- base-jamstack-sample/src/views/About.vue
+++ base-jamstack-sample/src/views/About.vue
@@ -1,11 +1,11 @@
 <template>
   <main>
-    <h1>This is an about page, work</h1>
+    <h1>This is an about page</h1>
     <p>
       Make your own layout! You can use this if it helps:
       <a href="https://cssgrid-generator.netlify.com/">CSS Grid Generator</a>
     </p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam alias quaerat dolores fugit, hic, recusandae qui assumenda, rem voluptates officia consequuntur
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam alias quaerat dolores fugit, hic, recusandae qui assumenda, rem voluptates officia consequntur
     perferendis. Id ex aperiam cumque expedita suscipit ab repellendus?</p>
   </main>
 </template>
